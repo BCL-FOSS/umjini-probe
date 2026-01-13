d10162d01d2d86769f0f3acb58b822da91df4af0
D.B <165270880+BCL-FOSS@users.noreply.github.com>
2026-01-13 16:23:20 +0545

    probe initialization fix

--- bcl_umj_probe/init_app.py
+++ bcl_umj_probe/init_app.py
@@ -4,27 +4,19 @@ import redis
 from utils.network_utils.ProbeInfo import ProbeInfo
 import logging
 from passlib.hash import bcrypt
-from utils.RedisDB import RedisDB
-import uuid
 import os
-from onetimesecret import OneTimeSecretCli
-from utils.EmailAlert import EmailAlert
 
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('passlib').setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=True)
-prb_db = RedisDB(hostname=os.environ.get('PROBE_DB'), port=os.environ.get('PROBE_DB_PORT'))
+
 probe_utils = ProbeInfo()
 
 r = redis.Redis(host=os.environ.get('PROBE_DB'), port=os.environ.get('PROBE_DB_PORT'), decode_responses=True)
 pong = r.ping()
 logger.info(f"Redis ping: {pong}")
-
-email_alert = EmailAlert()
-
-cli = OneTimeSecretCli(os.environ.get('OTS_USER'), os.environ.get('OTS_KEY'), os.environ.get('REGION'))
 
 def init_probe():
     prb_id, hstnm = probe_utils.gen_probe_register_data()
@@ -32,27 +24,6 @@ def init_probe():
 
     if not keys:
         probe_data=probe_utils.collect_local_stats(id=f"{prb_id}", hostname=hstnm)
-        api_key = uuid.uuid4()
-
-        link = cli.create_link(api_key,ttl=int(os.environ.get('OTS_TTL')))
-
-        email_alert.send_email(
-            subject="umjiniti Probe API Key Information",
-            body=f"""
-            Hello,
-
-            Your umjiniti Probe has been initialized successfully.
-
-            Please use the following link to retrieve your API key. Note that this link will expire in {os.environ.get('OTS_TTL')} seconds.
-
-            API Key Retrieval Link: {link}
-
-            Thank you,
-            umjiniti Team
-            """
-        )
-
-        probe_data['api_key'] = bcrypt.hash(str(api_key))
 
         str_hashmap = {str(k): str(v) for k, v in probe_data.items()}
         result = r.hset(prb_id, mapping=str_hashmap)
